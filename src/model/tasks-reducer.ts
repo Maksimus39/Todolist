872343629dd8ce32ef7caabcb39efc1dc5a6b19b
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-08-27 13:11:51 +0300

    дописал ActionType,AC и обработал это в редюсере

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -2,6 +2,7 @@
 import {TasksStateType} from "../App";
 import {v1} from "uuid";
 import {TasksType} from "../Todolist";
+import {AddTodolistActionType, RemoveTodolistActionType} from "./todolists-reducer";
 
 export type RemoveTasksActionType = {
     type: 'REMOVE-TASK',
@@ -15,6 +16,22 @@ export type AddTasksActionType = {
     payload: {
         title: string,
         todolistId: string
+    }
+}
+export type ChangeTaskStatusActionType = {
+    type: 'CHANGE-TASK-STATUS',
+    payload: {
+        todolistId: string,
+        taskID: string,
+        taskStatus: boolean
+    }
+}
+export type ChangeTaskTitleActionType = {
+    type: 'CHANGE-TASK-TITLE',
+    payload: {
+        todolistId: string
+        taskID: string
+        title: string
     }
 }
 // ActionCreator
@@ -36,9 +53,33 @@ export const addTaskAC = (title: string, todolistId: string): AddTasksActionType
         } as const
     }
 }
+export const changeTaskStatusAC = (todolistId: string, taskID: string, taskStatus: boolean): ChangeTaskStatusActionType => {
+    return {
+        type: 'CHANGE-TASK-STATUS',
+        payload: {
+            todolistId: todolistId,
+            taskID: taskID,
+            taskStatus: taskStatus
+        }
+    }
+}
+export const changeTaskTitleAC = (todolistId: string, taskID: string, title: string): ChangeTaskTitleActionType => {
+    return {
+        type: 'CHANGE-TASK-TITLE',
+        payload: {
+            todolistId: todolistId,
+            taskID: taskID,
+            title: title
+        }
+    }
+}
 
-
-export type ActionsTasksType = RemoveTasksActionType | AddTasksActionType
+export type ActionsTasksType = RemoveTasksActionType
+    | AddTasksActionType
+    | ChangeTaskStatusActionType
+    | ChangeTaskTitleActionType
+    | AddTodolistActionType
+    | RemoveTodolistActionType
 
 let todolistID1 = v1()
 let todolistID2 = v1()
@@ -71,6 +112,29 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
             return {
                 ...state, [action.payload.todolistId]: [newTask, ...state[action.payload.todolistId]],
             }
+        }
+        case "CHANGE-TASK-STATUS":
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].map(ts => ts.id === action.payload.taskID ? {
+                    ...ts,
+                    isDone: action.payload.taskStatus
+                } : ts)
+            }
+        case "CHANGE-TASK-TITLE":
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].map(ts => ts.id === action.payload.taskID ? {
+                    ...ts,
+                    title: action.payload.title
+                } : ts)
+            }
+        case "ADD-TODOLIST":
+            return {...state, [action.payload.todolistId]: []}
+        case "REMOVE-TODOLIST": {
+            const stateCopy = { ...state };
+            delete stateCopy[action.payload.id];
+            return stateCopy;
         }
         default:
             throw new Error("I don't understand this type")
